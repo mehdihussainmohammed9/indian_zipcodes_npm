f64d3749b53440e12c295967a5636315687c7499
hussain9 <hussain@databeat.io>
2021-09-25 20:44:35 +0530

    commit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-var getData = require("./getInfo");
-getData("500081");
+const fetch = require("node-fetch");
+
+function ZipcodeData(string) {
+  fetch("https://api.postalpincode.in/pincode/" + string)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res[0].PostOffice);
+      return res[0].PostOffice;
+    });
+}
+
+module.exports = ZipcodeData();
