2625783a8d7ed5956be4f1692b5579a9ed61f1d8
hussain9 <hussain@databeat.io>
2021-09-25 20:28:42 +0530

    commit

--- getInfo.js
+++ getInfo.js
@@ -1,7 +1,6 @@
 const fetch = require("node-fetch");
 
 module.exports = function zipcode(Zipcode) {
-  console.log("This is a message from the demo package", Zipcode);
   var k = fetch("https://api.postalpincode.in/pincode/" + Zipcode)
     .then((res) => res.json())
     .then((res) => {
